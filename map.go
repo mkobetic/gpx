9571531367226c8f53e86f6453c41ac4e108616d
Martin Kobetic <magh@kobetic.ca>
2025-04-14 13:27:46 -0400

    Zooming and panning for the map

--- map.go
+++ map.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"math"
 	"time"
@@ -9,34 +8,34 @@ import (
 	"github.com/tkrajina/gpxgo/gpx"
 )
 
-const border = 0.05
+const border = 20 // points of SVG coordinates
 
 // Map is used to translate track coordinates into SVG coordinate system for rendering.
 type Map struct {
-	w, h   int     // widht, height in svg coordinates
+	w, h   float64 // width, height in svg coordinates derived from lw/lh
 	lw, lh float64 // width, height in lat/lon degrees
 	lx, ly float64 // bottom/left offset in lat/lon degrees
 	coef   float64 // longitudinal adjustment coeficient (degrees of longitude are shorter in higher latitudes)
 }
 
-func NewMap(b gpx.GpxBounds, width int) *Map {
-	m := &Map{w: width, lx: b.MinLongitude, ly: b.MinLatitude}
+func NewMap(b gpx.GpxBounds, unit unit) *Map {
+	m := &Map{lx: b.MinLongitude, ly: b.MinLatitude}
+	// calculate the coeficient for longitudinal adjustment
 	m.coef = math.Cos((b.MaxLatitude + b.MinLatitude) * math.Pi / 360)
+	// calculate lat/long dimensions
 	m.lw = (b.MaxLongitude - b.MinLongitude) * m.coef
 	m.lh = b.MaxLatitude - b.MinLatitude
-	bx := border * m.lh
-	m.lh += 2 * bx
-	m.lx -= bx
-	m.lw += 2 * bx
-	m.ly -= bx
-	m.h = int(m.lh / m.lw * float64(m.w))
+	// compute SVG dimensions as lat/long dimensions * unit
+	// i.e. one point in SVG coordinates is 1 unit
+	m.w = m.lw*m.coef*float64(unit) + 2*border
+	m.h = m.lh/m.lw*float64(m.w) + 2*border
 	return m
 }
 
 // Point translates a GPS point into SVG coordinates.
 func (m *Map) Point(p *gpx.GPXPoint) (x, y int) {
-	y = m.h - int((p.Latitude-m.ly)/m.lh*float64(m.h))
-	x = int((p.Longitude - m.lx) * m.coef / m.lw * float64(m.w))
+	y = int(m.h-(p.Latitude-m.ly)/m.lh*float64(m.h)) + border
+	x = int((p.Longitude-m.lx)*m.coef/m.lw*float64(m.w)) + border
 	return
 }
 
@@ -94,13 +93,4 @@ func (m *Map) SpeedColor(speed float64) string {
 		s = len(palette) - 1
 	}
 	return fmt.Sprintf("#%03x", palette[s])
-}
-
-func (m *Map) polylinePoints(s *Segment) string {
-	b := bytes.NewBuffer(nil)
-	for i := range s.gpx.Points {
-		x, y := m.Point(s.gpxPoint(i))
-		fmt.Fprintf(b, "%d,%d ", x, y)
-	}
-	return b.String()
 }
